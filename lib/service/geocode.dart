38a2ec60802bc07721b291ae6b43872c946daec1
Tristan Marsell <tristan@sqrls.de>
2019-04-09 23:29:18 +0200

    Geocode: Update location library

--- lib/service/geocode.dart
+++ lib/service/geocode.dart
@@ -3,7 +3,16 @@ import 'package:location/location.dart';
 class DesireDriveGeocode {
   Future<Map<String, double>> location() async {
     var locationInit = new Location();
-    return await locationInit.getLocation();
+    return await interpreter(locationInit.getLocation());
+  }
+
+  Future<Map<String, double>> interpreter(Future<LocationData> location) async {
+    return location.then((res) {
+      return {
+        'latitude': res.latitude,
+        'longitude': res.latitude
+      };
+    });
   }
 
   Future<double> latitude() async {
